98d0eda3a0d8c501c32a2d6f5d06a130d47c2804
hridyacp <jithridya@gmail.com>
2024-07-06 11:21:42 +0530

    Add styles

--- src/pages/Result/Result.js
+++ src/pages/Result/Result.js
@@ -40,45 +40,41 @@ const Result = () => {
     }
 
     return (
-        <div className="App">
-            <div className="result-container">
-                {/* <div className="answered-percent">
-                    <div className="correct-answer">
-                        <FontAwesomeIcon icon={faCheck} />{correctAnswer}<div className="divide"></div> {totalAnswered}%
-                    </div>
-                    <div className="wrong-answer">
-                        <FontAwesomeIcon icon={faXmark} />{wrongAnswer}<div className="divide"></div> {wrongPercent}%
-                    </div>
-                </div> */}
-             
+        // <div className="App">
+            <div className="result-container">   
+                <div className="box-container">
+                <Grid container className="total-result">
+                    <Grid item xs={6}>i. Passing score is 80%.</Grid>
+                </Grid>
                 <div className="message-container">
-                <div className="inspire">Great Work!!</div>
-                <div>
-                {isPassed ?
-                    <div className="passed">Congratulations!! You have cleared the test.<div>You really 'mapped' out that quiz! Great job!</div>
-                        <div className="retake"><div className="link" onClick={retakeTest}> Take test again.</div> <div className="link" onClick={checkAnswers}>Check answers</div></div> </div> :
+                <div className="inspire">GREAT WORK!!</div>
+              
+                {isPassed ?<>
+                 <div className="inspire">YOU WIN!!</div>
+                    <div className="passed"><div>You really 'mapped' out that quiz! Great job!</div></div></> :
                     <div className="failed">You are almost there. <div>Don't worry, even great explorers got lost sometimes. Keep charting your course!</div>
-                        <div className="retake"><div className="link" onClick={retakeTest}> Take test again.</div> <div className="link" onClick={checkAnswers}> Check answers </div></div></div>}
+                      </div>}
                         </div>
-                        </div>
-                        <Grid container className={"answered-result"} justifyContent={"center"} alignItems={"center"}>
-                    <Grid item xs={6} md={3} >
+                     
+                        <Grid container className={"answered-result"} justifyContent={"center"} alignItems={"center"} spacing={2}>
+                    <Grid item xs={6} md={6} >
                         Answered: {state?.questionsList.filter((answer) => answer?.isAnswered === true).length}
                     </Grid>
-                    <Grid item xs={6} md={3}>
+                    <Grid item xs={6} md={6}>
                         Skipped: {state?.questionsList?.length - state?.questionsList.filter((answer) => answer?.isAnswered === true).length}
                     </Grid>
-                    <Grid item xs={6} md={3}>
+                    <Grid item xs={6} md={6}>
                        Correct Answer: {correctAnswer}
                     </Grid>
-                    <Grid item xs={6} md={3}>
+                    <Grid item xs={6} md={6}>
                        Percentage: {totalAnswered}%
                     </Grid>
+                    <Grid item xs={12} md={6} >
+                <button className="link" onClick={retakeTest}> Take test again.</button>
                 </Grid>
-
-                <Grid container className="total-result">
-                    <Grid item xs={6}>{correctAnswer} out of {state?.questionsList?.length} answered correctly.</Grid> 
-                    <Grid item xs={6}>Passing score is 80%.</Grid>
+                <Grid item xs={12} md={6}>
+                 <button className="link" onClick={checkAnswers}>Check answers</button>
+                 </Grid>
                 </Grid>
                         <div>
                 {isCheck ?
@@ -92,8 +88,10 @@ const Result = () => {
                                     <div className='options-contain'>
                                         {quest?.options?.map((item, index) => (
                                             <div className={item?.isCorrect ? `options-div correct` : item?.isSelected && !item?.isCorrect ? 'options-div incorrect' : 'options-div'} key={index}>
-                                             <div className="options" style={{justifyContent:"space-around",width:"50%"}}> <div>{(index + 1 + 9).toString(36)}</div>
-                                                    <div>{item?.label}</div> </div> {item?.isCorrect?<FontAwesomeIcon icon={faCheck} />:item?.isSelected && !item?.isCorrect?<FontAwesomeIcon icon={faXmark} />:<FontAwesomeIcon icon={faXmark} style={{visibility:"hidden"}}/>}    
+                                             <div className="option" style={{justifyContent:"space-around",width:"50%"}}> <div>{(index + 1 + 9).toString(36)}</div>
+                                                    <div>{item?.label}</div> 
+                                                    </div>
+                                                     {item?.isCorrect?<FontAwesomeIcon icon={faCheck} />:item?.isSelected && !item?.isCorrect?<FontAwesomeIcon icon={faXmark} />:<FontAwesomeIcon icon={faXmark} style={{visibility:"hidden"}}/>}    
                                             </div>
                                         ))}
                                     </div>
@@ -103,8 +101,9 @@ const Result = () => {
                      </Grid>
                     : ""}
                     </div>
+                    </div> 
             </div>
-        </div>
+        // </div>
     )
 }
 export default Result;
